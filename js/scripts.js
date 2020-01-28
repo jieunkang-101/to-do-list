6982b3da9eba6a36eed4ab0b6b64c2b1470c33ea
Jieun Kang <jieunkang101@gmail.com>
2020-01-27 21:12:00 -0800

    add checkbox

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,36 @@
-//Business logic for To Do List
+//Business logic =================================
 function ToDoList() {
   this.todos = [];
   this.currentId = 0;
 }
 
 ToDoList.prototype.addToDoList = function(todo) {
-  todo.id - this.assignId();
+  todo.id = this.currentId;
   this.todos.push(todo);
+  this.currentId++;
 }
 
-ToDoList.prototype.assignId = function() {
-  this.currentId +=1 ;
-  return this.currentId;
-}
-
-ToDoList.prototype.findToDo = function(id) {
-  for (var i=0; i< this.todos.length; i++) {
-    if (this.todos[i]) {     
-      if (this.todos[i].id == id) {
-        return this.todos[i];
-      }
-    }                          
-  };
-  return false;
-}
-
-//Business logic for To Do
 function ToDo(notes) {
   this.notes = notes;
 }
 
-
-
-
-
-// User interface logic
-var toDoList = new ToDoList();
-
-function displayToDoList(toDoListToDisplay) {
-  var toDoDisplay = $("ul#checkbox");
-  var htmlForToDo = "";
-  toDoListToDisplay.todos.forEach(function(todo) {
-    var x = document.createElement("INPUT");
-    x.setAttribute("type", "checkbox");
-    htmlForToDo +=  x + "<li id=" + todo.id + ">" + todo.notes + "</li>"
-  });
-  toDoDisplay.html(htmlForToDo);
-};
-
-function showToDo(toDoId) {
-  var todo = toDoList.findToDo(toDoId);
-  $("#output").show();
-  $("#checkbox").html(todo.notes);
-}
-
-
+//User Interface ==================================
 $(document).ready(function() {
+  var toDoList = new ToDoList();
   $("form#userInput").submit(function(event){
     event.preventDefault();
     var inputtedToDo = $("input#thingsToDo").val();
-    $("input#thingsToDo").val();
+    //$("input#thingsToDo").val();
     var newToDo = new ToDo (inputtedToDo);
     toDoList.addToDoList(newToDo);
-    $("#output").show(displayToDoList(toDoList));
+    $("#done").append("<li><input id='" + newToDo.id + "' type='checkbox'>" +  " " + newToDo.notes + "</li>");
+    $("form").val("");
+  });
+
+  $("#done").on("click", "input[type='checkbox']", function() {
+    // var id = parseInt(event.target.id);
+    // console.log(id);
+    //$(this).parent().detach();
+    $(this).parent().checked();
   });
 });
